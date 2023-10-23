f3b61afd41e4431fdbe806b033876ec98ffcc26b
Blake Costa <blakecosta117@gmail.com>
2023-10-23 18:46:37 -0400

    Add started code for lecture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
     println!("Hello, Blake!");
+
+    // Comments
+    // THIS IS A COMMENT
+    /* THIS IS A MULTILINE COMMENTS */
+
+    // Variables and Basic Data Types
+    let age: u32 = 5; // cant be negative
+    let height: f64 = 5.10;
+    let a_boolean: bool = false;
+    let first_letter_of_name: char = 'B';
+    let a_statement: String = String::from("I don't like the letter B");
+    let b_statement: String = "I love sunsets over the mountains".to_string();
+    println!(
+        "age: {}\nheight: {}\na_boolean: {}\nfirst_letter_of_name: {}\na_statement: {}\nb_statement: {}",
+        age, height, a_boolean, first_letter_of_name, a_statement, b_statement
+    );
+
+    // ASSSIGNMENT:
+    // Initialize 5 new variables based on the following data types:
+    // 1. a string
+    // 2. an integer
+    // 3. a float
+    // 4. a boolean
+    // 5. a char
+    let new_person: String = String::from("Ezekiel");
+    let new_age: u32 = 20;
+    let new_height_meters: f64 = 1.85; // this is ~6.1.
+    let likes_tacos: bool = true;
+    let new_first_letter_of_name: char = 'E';
+    println!(
+        "new_person: {}\nnew_age: {}\nnew_height_meters: {}\nlikes_tacos: {}\nnew_first_letter_of_name: {}",
+        new_person, new_age, new_height_meters, likes_tacos, new_first_letter_of_name
+    );
 }
