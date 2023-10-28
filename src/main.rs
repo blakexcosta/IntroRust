8747558174f44a44fe6741d271be58bb8238d99d
Blake Costa <blakecosta117@gmail.com>
2023-10-28 12:29:48 -0400

    Lecture and starter assignment code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // PART 1 - 2
     println!("Hello, Blake!");
 
     // Comments
@@ -11,20 +12,22 @@ fn main() {
     let a_boolean: bool = false;
     let first_letter_of_name: char = 'B';
     let a_statement: String = String::from("I don't like the letter B");
-    let b_statement: String = "I love sunsets over the mountains".to_string();
+    let mut b_statement: String = "I love sunsets over the mountains".to_string();
     println!(
         "age: {}\nheight: {}\na_boolean: {}\nfirst_letter_of_name: {}\na_statement: {}\nb_statement: {}",
         age, height, a_boolean, first_letter_of_name, a_statement, b_statement
     );
 
+    // PART 3
+    // uncomment and show this erroring out
+    // a_statement = String::from("I do like the letter B"); // this will error out
+    // println!("b_statement: {}", b_statement);
+    b_statement = String::from("I don't like sunsets");
+    println!("b_statement: {}", b_statement);
+
     // ASSSIGNMENT:
-    let new_person: String = String::from("Ezekiel");
-    let new_age: u32 = 20;
-    let new_height_meters: f64 = 1.85; // this is ~6.1.
-    let likes_tacos: bool = true;
-    let new_first_letter_of_name: char = 'E';
-    println!(
-        "new_person: {}\nnew_age: {}\nnew_height_meters: {}\nlikes_tacos: {}\nnew_first_letter_of_name: {}",
-        new_person, new_age, new_height_meters, likes_tacos, new_first_letter_of_name
-    );
+    // 1. Reassign Variables with the 'mut' keyword
+    // 2. Print out variables before and after reassignment
+    let num1: u32 = 5;
+    let string1: String = String::from("hello");
 }
