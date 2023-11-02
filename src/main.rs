7537e5dbc6d8600cf6859ac4779112a18ad7d5d1
Blake Costa <blakecosta117@gmail.com>
2023-11-02 15:47:41 -0400

    Assignment solution

--- src/main.rs
+++ src/main.rs
@@ -58,10 +58,27 @@ fn main() {
     // Practical
     // 1. Iterate over this vector and print out the values:
     let names = vec!["Bob", "Frank", "Ferris"];
+    for name in &names {
+        println!("{}", name);
+    }
 
     //2. Use a while loop to print the values 0 to 100 and add each element to a vector
     // HINT: Use type annotations when declaring a new vector (in the format of Vec<T>)
+    let mut counter = 0;
+    let mut new_vec: Vec<i32> = Vec::new();
+    //set our looping condition
+    while counter < 101 {
+        // add element to vector
+        new_vec.push(counter);
+        // increment counter for condtion
+        counter += 1;
+    }
 
     //3. use an if statement to check whether the altitude is > 5000
     let altitude = 6000;
+    if altitude > 5000 {
+        println!("Altitude is greater than 5000");
+    } else {
+        println!("Altitude is less than 5000");
+    }
 }
