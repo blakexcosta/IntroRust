ce4f5b47d5b67af4fbffc42dcf6d70e2b0d79cbf
Blake Costa <blakecosta117@gmail.com>
2023-11-06 16:49:52 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,15 @@
 fn main() {
-    // PART 5 CONTROL FLOW
+    // PART 6 FUNCTIONS - FINAL
 
     // ----------------------------------------
-    // IF
-    let number = 1000;
-    if number > 100 {
-        println!("Number is greater than 100");
-    } else {
-        println!("Number is not greater than 100");
-    }
-    // else if
-    let num = -50;
-    if num > 0 {
-        println!("Number is positive");
-    } else if num < 0 {
-        println!("Number is negative");
-    } else {
-        println!("Number is zero");
-    }
-    // ----------------------------------------
-    // LOOPS
-
-    //while loop
-    //declare counter variable
-    let mut counter = 1;
-    //set our looping condition
-    while counter < 6 {
-        println!("{}", counter);
-        // increment counter for condtion
-        counter += 1;
-    }
-
-    // for loops
-    // inclusive range
-    for number in 1..=5 {
-        println!("for loop inclusive: {}", number);
-    }
-    // exclusive range
-    for number in 1..5 {
-        println!("for loop exclusive: {}", number);
-    }
-    // looping over a vector
-    let numbers = vec![1, 2, 3, 4, 5];
-    for number in &numbers {
-        println!("{}", number);
-    }
 
     // ----------------------------------------
     // ASSSIGNMENT:
-    // Quiz:
-    // 1. is `..=` inclusive or exclusive when in a for loop?
-    // 2. What will the last println! of the following code be?
-    // for number in 1..12 {
-    //     println!("{}", number);
-    // }
+    // 1. Create a function that prints your name
 
-    // Practical
-    // 1. Iterate over this vector and print out the values:
-    let names = vec!["Bob", "Frank", "Ferris"];
+    // 2. Call the new name function in main()
 
-    //2. Use a while loop to print the values 0 to 100 and add each element to a vector
-    // HINT: Use type annotations when declaring a new vector (in the format of Vec<T>)
+    // 3. create a function that adds two f64's together and returns a f64
 
-    //3. use an if statement to check whether the altitude is > 5000
-    let altitude = 6000;
+    // 4. Call the new f64 function in main(), store the result in a variable, and print that variable to the console
 }
